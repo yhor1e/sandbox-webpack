536092d32350dc25c02fbb94086d5c946eabbb0e
yhorie <yuk.horie@gmail.com>
2021-02-10 23:28:51 +0900

    Modify to TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,23 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].bundle.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    //    extensions: ['.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
